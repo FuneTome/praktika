e2baf349427ac1a3da93672075f3838683328cf6
FuneTome <kurilenkof@gmail.com>
2024-06-29 01:11:56 +0300

    меню и первый пункт

--- praktica/Source.cpp
+++ praktica/Source.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void mass_create(int n, int mass[]) {
-	srand(time(0));
-	for (int i = 0; i < n; i++) {
-		mass [i] = rand() % 201 - 100;
-	}
-}
 void choice_sort(int n, int mass[]) {
 	int min_i = 0;
 	for (int i = 0; i < n; i++) {
@@ -21,18 +16,55 @@ void choice_sort(int n, int mass[]) {
 		}
 	}
 }
+void create_file(int n) {
+	ofstream fout("no_sort_array.txt");
+	srand(time(0));
+	for (int i = 0; i < n; i++) {
+		fout << rand() % 20001 - 10000 << endl;
+	}
+	fout.close();
+}
+void menu() {
+	int n;
+	cout << "\n\t1. Случайный ввод\n\t2. Ручной ввод\n\t3. Ввод с файла\n\t4. Выход\n\tВыберите пункт: ";
+	cin >> n;
+	switch (n) {
+	case 1: {
+		int n, buf = 0;
+		cout << "Введите количество элементов: ";
+		cin >> n;
+		int* mass = new int[n];
+		create_file(n);
+		ifstream fin("no_sort_array.txt");
+		for (int i = 0; i < n; i++) {
+			fin >> mass[i];
+		}
+		fin.close();
+		time_t start = clock();
+		choice_sort(n, mass);
+		time_t stop = clock();
+		double time = (stop - start) / 1000.0;
+		cout << time;
+		ofstream fout("sort_array.txt");
+		for (int i = 0; i < n; i++) {
+			fout << mass[i] << endl;
+		}
+		fout.close();
+	}break;
+	case 2: {
+		int n;
+		cout << "Введите количество элементов: ";
+		cin >> n;
+		int* mass = new int[n];
+	}break;
+	case 3: {}break;
+	case 4: { exit(0); } break;
+	default:{ cout << "Такого пункта нет, попробуйте ещё раз" << endl; }break;
+	}
+}
 
 int main(){
 	setlocale(0, "");
-	int n;
-	cout << "Введите количество элементов: ";
-	cin >> n;
-	int* mass = new int[n];
-	mass_create(n, mass);
-	time_t start = clock();
-	choice_sort(n, mass);
-	time_t stop = clock();
-	double time = (stop - start) / 1000.0;
-	cout << time;
+	menu();
 	return 0;
 }
