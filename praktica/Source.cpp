bba4ff0ee87d7dc78afdc39ac4be0256144e475e
FuneTome <kurilenkof@gmail.com>
2024-06-29 23:35:14 +0300

    оставшая часть меню и полностью готовый продукт

--- praktica/Source.cpp
+++ praktica/Source.cpp
@@ -25,13 +25,16 @@ void create_file(int n) {
 	fout.close();
 }
 void menu() {
-	int n;
-	cout << "\n\t1. Случайный ввод\n\t2. Ручной ввод\n\t3. Ввод с файла\n\t4. Выход\n\tВыберите пункт: ";
+	int n, count;
+	string name, t = ".txt";
+	cout << "\n\t1. Случайный ввод\n\t2. Ручной ввод\n\t3. Ввод с файла\n\t4. Выход\n\n\tВыберите пункт: ";
 	cin >> n;
+	cout << endl;
+	while (true){
 	switch (n) {
 	case 1: {
 		int n, buf = 0;
-		cout << "Введите количество элементов: ";
+		cout << "\tВведите количество элементов: ";
 		cin >> n;
 		int* mass = new int[n];
 		create_file(n);
@@ -44,23 +47,62 @@ void menu() {
 		choice_sort(n, mass);
 		time_t stop = clock();
 		double time = (stop - start) / 1000.0;
-		cout << time;
+		cout << "\n\tВремя сортировки массива состовляет: " << time << " секунд\n";
 		ofstream fout("sort_array.txt");
 		for (int i = 0; i < n; i++) {
 			fout << mass[i] << endl;
 		}
 		fout.close();
-	}break;
+		exit(0);
+	}
 	case 2: {
 		int n;
-		cout << "Введите количество элементов: ";
+		cout << "\tВведите количество элементов: ";
 		cin >> n;
+		cout << endl;
 		int* mass = new int[n];
-	}break;
-	case 3: {}break;
-	case 4: { exit(0); } break;
-	default:{ cout << "Такого пункта нет, попробуйте ещё раз" << endl; }break;
+		for (int i = 0; i < n; i++) {
+			cout << "\tВведите " << i + 1 << " элемент массива: ";
+			cin >> mass[i];
+			cout << endl;
+		}
+		time_t start = clock();
+		choice_sort(n, mass);
+		time_t stop = clock();
+		double time = stop - start;
+		cout << "\tВремя сортировки массива состовляет: " << time << " милисекунд\n\n" << "Сортированный массив: ";
+		for (int i = 0; i < n; i++) {
+			cout << mass[i] << " ";
+		}
+		exit(0);
 	}
+	case 3: {
+		cout << "\tВведите имя файла: ";
+		cin >> name;
+		name += t;
+		ifstream fin(name);
+		fin >> count;
+		int* mass = new int[count];
+		for (int i = 0; i < count; i++) {
+			fin >> mass[i];
+		}
+		fin.close();
+		time_t start = clock();
+		choice_sort(count, mass);
+		time_t stop = clock();
+		double time = (stop - start) / 1000.0;
+		cout << "\n\tВремя сортировки массива состовляет: " << time << " секунд";
+		ofstream fout("sort_array.txt");
+		for (int i = 0; i < count; i++) {
+			fout << mass[i] << endl;
+		}
+		fout.close();
+		exit(0);
+	}
+	case 4: { exit(0); } break;
+	default: { cout << "Такого пункта нет, попробуйте ещё раз" << endl; }break;
+	}
+}
 }
 
 int main(){
